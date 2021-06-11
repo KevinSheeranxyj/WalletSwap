14b572960ade815555e692396a76ae5ee14c3e50
Henry Young <henry.young@dtc.top>
2021-06-11 12:43:02 +0800

    UPDATE: SLIP-44 constant

---
+++ src/main/java/top/dtc/crypto_cli/slip/SLIP0044.java
@@ -0,0 +1,9 @@
+package top.dtc.crypto_cli.slip;
+
+public class SLIP0044 {
+
+    public static final int BTC = 0;
+    public static final int ETH = 60;
+    public static final int TRX = 195;
+
+}
