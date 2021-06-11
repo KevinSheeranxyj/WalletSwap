496e6ea9729bc2ca93fca756c83b2cfd7e142bfa
Henry Young <henry.young@dtc.top>
2021-06-11 12:46:37 +0800

    UPDATE: ForApiTest more tests

--- src/test/java/top/dtc/crypto_cli/ForApiTest.java
+++ src/test/java/top/dtc/crypto_cli/ForApiTest.java
@@ -4,9 +4,10 @@ import com.google.common.io.BaseEncoding;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import top.dtc.crypto_cli.bip.*;
+import top.dtc.crypto_cli.slip.SLIP0044;
 import top.dtc.crypto_cli.util.Base58;
+import top.dtc.crypto_cli.util.Sha256Hash;
 
-import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
@@ -16,20 +17,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ForApiTest {
 
     @BeforeAll
-    public static void init() throws IOException {
+    public static void init() {
         BIP0039.init();
     }
 
-    // Test data generated on https://iancoleman.io/bip39/
+    // Test data verified on https://iancoleman.io/bip39/
+
+    /**
+     * IMPORTANT: DO NOT USE THESE CREDENTIALS FOR ANY USAGE
+     */
     @Test
     public void go() throws NoSuchAlgorithmException, InvalidKeySpecException {
         byte[] entropy = BaseEncoding.base16().decode("BA43B688194E3651380DDF778D2119EFDF16DAF8F2446117A19CE3A58D1F3790");
         String[] mnemonics = BIP0039.genMnemonics(entropy);
         assertEquals(
-                "ripple buffalo pear crater toddler chuckle then room jealous harbor edit text vapor hope toilet embrace genre rug grunt shrug flee moon tattoo black",
+                "ripple buffalo pear crater toddler chuckle then room jealous harbor edit text" + " " + "vapor hope toilet embrace genre rug grunt shrug flee moon tattoo black",
                 String.join(" ", mnemonics)
         );
         assertArrayEquals(entropy, BIP0039.toBytes(mnemonics));
+        byte[] entropyHash = Sha256Hash.hashTwice(entropy);
+        String entropyHashStr = Base58.encode(entropyHash).substring(0, 8);
+        assertEquals("E5H9UAbq", entropyHashStr);
         byte[] seed = BIP0039.genSeed(mnemonics, "mMpc]HXW&:$98;7<");
         assertEquals(
                 "873D7C49734A5FF7FBD30953910B0C9A9DD8207DD3D9ADF42491BC362C60386D54C27ABD32E5913CC32EFCAEF1F2FC974376D26A4432DB13F098BC5C4D1B2578",
@@ -42,29 +50,55 @@ public class ForApiTest {
                 Base58.encode(xprv_master)
         );
 
-        byte[] xprv_0_0_0_0 = BIP0044.derive(xprv_master, 0, 0, true, 0);
+        byte[] xprv_0_0_0_0 = BIP0044.derive(xprv_master, SLIP0044.BTC, 0, true, 0);
         byte[] pub_0_0_0_0 = BIP0032.toPublicKey(BIP0032.genHdPublicKey(xprv_0_0_0_0));
         assertEquals("1u8YwPPFBaVfJRDhiFhx61ZuFvMTFM4y9", Base58.encode(BIP0013.genAddress(pub_0_0_0_0)));
         assertEquals("035AEE2CAB7F404D65B7B10364DA8B9184907CF38408806C79B7F3FC56C4ED8790", BaseEncoding.base16().encode(pub_0_0_0_0));
         assertEquals("KzxrDKaoUF2AyptoT87Aj9zXdoWFvdTMgLdYSy4svZcF84FHxgDi", Base58.encode(BIP.wif(BIP0032.toPrivateKey(xprv_0_0_0_0))));
 
-        byte[] xprv_0_0_0_1 = BIP0044.derive(xprv_master, 0, 0, true, 1);
+        byte[] xprv_0_0_0_1 = BIP0044.derive(xprv_master, SLIP0044.BTC, 0, true, 1);
         byte[] pub_0_0_0_1 = BIP0032.toPublicKey(BIP0032.genHdPublicKey(xprv_0_0_0_1));
         assertEquals("1J1EGjY53TP2otXMYiRp1z32uu4hyvYTFW", Base58.encode(BIP0013.genAddress(pub_0_0_0_1)));
         assertEquals("031F03992426C2917C3F58365BE6DE30C6A83752DD36112DF377995056A6FFA21F", BaseEncoding.base16().encode(BIP0032.toPublicKey(BIP0032.genHdPublicKey(xprv_0_0_0_1))));
         assertEquals("L3EzJ6ShHvNrTRaMpoKmmQSy4Raq1h8byAXwMCsYutfM9woNNQc7", Base58.encode(BIP.wif(BIP0032.toPrivateKey(xprv_0_0_0_1))));
 
-        byte[] xprv_196_761273_0_0 = BIP0044.derive(xprv_master, 196, 761273, true, 0);
-        byte[] pub_196_761273_0_0 = BIP0032.toPublicKey(BIP0032.genHdPublicKey(xprv_196_761273_0_0));
+        byte[] xprv_60_761273_0_0 = BIP0044.derive(xprv_master, SLIP0044.ETH, 761273, true, 0);
+        byte[] xpub_60_761273_0_0 = BIP0032.genHdPublicKey(xprv_60_761273_0_0);
+        byte[] pub_60_761273_0_0 = BIP0032.toPublicKey(xpub_60_761273_0_0);
+        assertEquals("xpub6FsM36iUKuQBkcnKpNy84yAE92hqAqiruFqDpPMALj1gMu9auS3wjjFR1moGApANA4jdXzZ6eZ1vGHhpwNUyvkiPudz9gVN7UdyUpFW7RGM", Base58.encode(xpub_60_761273_0_0));
+        assertEquals("1Dd1YEtn5V7TwgzAkJPX4NXM4MSrMniKDP", Base58.encode(BIP0013.genAddress(pub_60_761273_0_0)));
+        assertEquals("02FBADA42F5B6BCB8CAFFA188FD544D205B87D588246BE076CCBA7EB845BC26FDF", BaseEncoding.base16().encode(pub_60_761273_0_0));
+        assertEquals("Kyf6TjEZoBffTyEThFzLy5pEwoRepsNEjGupC5ThNjhjLPiuaU4i", Base58.encode(BIP.wif(BIP0032.toPrivateKey(xprv_60_761273_0_0))));
+
+        byte[] xprv_60_761273_0_516717 = BIP0044.derive(xprv_master, SLIP0044.ETH, 761273, true, 516717);
+        byte[] xpub_60_761273_0_516717 = BIP0032.genHdPublicKey(xprv_60_761273_0_516717);
+        byte[] pub_60_761273_0_516717 = BIP0032.toPublicKey(BIP0032.genHdPublicKey(xprv_60_761273_0_516717));
+        byte[] prv_60_761273_0_516717 = BIP0032.toPrivateKey(xprv_60_761273_0_516717);
+        assertEquals("xpub6FsM36iUL29gJe1oBZK14B7HKQjuDYHmTsPMNiBYS4BU5e6gis55mi7H81xNkay1hh47b9y1H1x5NCwEZECDHevZbykoG3DyWvcXbEMBue4", Base58.encode(xpub_60_761273_0_516717));
+        assertEquals("1J1rdzh8AyAE9f3JA19QVjLqCgzazsoMP", Base58.encode(BIP0013.genAddress(pub_60_761273_0_516717)));
+        assertEquals("0360D38882F743C490AA9A825882BB83045A84753A3E52047BFC7C159F582FFA64", BaseEncoding.base16().encode(pub_60_761273_0_516717));
+        assertEquals("KycGaonAshyFZZqCCDtANezFHEq9Wxgae1vmNgMBSmB1yUkgimte", Base58.encode(BIP.wif(prv_60_761273_0_516717)));
+        assertEquals("4747A098B03A770201CA2819EA5DDA99CC7A06ED6F364050DD97D56C3E139EE2", BaseEncoding.base16().encode(prv_60_761273_0_516717));
+
+        byte[] xprv_196_761273_0_0 = BIP0044.derive(xprv_master, SLIP0044.TRX + 1, 761273, true, 0);
+        byte[] xpub_196_761273_0_0 = BIP0032.genHdPublicKey(xprv_196_761273_0_0);
+        byte[] pub_196_761273_0_0 = BIP0032.toPublicKey(xpub_196_761273_0_0);
+        byte[] prv_196_761273_0_0 = BIP0032.toPrivateKey(xprv_196_761273_0_0);
+        assertEquals("xpub6GoVdEf6PMAqdj62CQX2JaeGyxPXzFGqDh1sGzNwuKa8WNT2Q1mLr9LSTgYhi3iQDBapKdaDUPohM89bQiVQJicQw6Pj8wR99H4xjmvocPn", Base58.encode(xpub_196_761273_0_0));
         assertEquals("16KvpA4BdujLqb4qvrpokpnXYKn7iis3Ld", Base58.encode(BIP0013.genAddress(pub_196_761273_0_0)));
         assertEquals("0343E67042DF68930ABB17DF52760EAAA4FA6FBCD1519A88489CFD6CEC2ABC8B30", BaseEncoding.base16().encode(pub_196_761273_0_0));
-        assertEquals("KwmcWvs3eKEcnfeHoJZ8Hw2MuuU6CDCFWzT2H5HGffaHFXifdhFf", Base58.encode(BIP.wif(BIP0032.toPrivateKey(xprv_196_761273_0_0))));
+        assertEquals("KwmcWvs3eKEcnfeHoJZ8Hw2MuuU6CDCFWzT2H5HGffaHFXifdhFf", Base58.encode(BIP.wif(prv_196_761273_0_0)));
+        assertEquals("106967E0302F8B26374DD383CE17F28DA718DC1706D8334D43A5979CC1C41F4B", BaseEncoding.base16().encode(prv_196_761273_0_0));
 
-        byte[] xprv_196_761273_0_516717 = BIP0044.derive(xprv_master, 196, 761273, true, 516717);
-        byte[] pub_196_761273_0_516717 = BIP0032.toPublicKey(BIP0032.genHdPublicKey(xprv_196_761273_0_516717));
+        byte[] xprv_196_761273_0_516717 = BIP0044.derive(xprv_master, SLIP0044.TRX + 1, 761273, true, 516717);
+        byte[] xpub_196_761273_0_516717 = BIP0032.genHdPublicKey(xprv_196_761273_0_516717);
+        byte[] pub_196_761273_0_516717 = BIP0032.toPublicKey(xpub_196_761273_0_516717);
+        byte[] prv_196_761273_0_516717 = BIP0032.toPrivateKey(xprv_196_761273_0_516717);
+        assertEquals("xpub6GoVdEf6PTvL9mwAETj4L6DAictCVFVAziLYA6sxAHv5resw9gP6S5a8iALfAXomprPa4mktJTzuXo7C7bExRh5A1EcPDzf7e5qUsVzYhTp", Base58.encode(xpub_196_761273_0_516717));
         assertEquals("1PUvLftcH5BB92qZsN1PG6VtCCh4hgp8UA", Base58.encode(BIP0013.genAddress(pub_196_761273_0_516717)));
         assertEquals("03FCD7DB3BC0556A3E4B9455D0DA34D041A1841B1E0743FF096347137841C1D552", BaseEncoding.base16().encode(pub_196_761273_0_516717));
-        assertEquals("L4vDVKWMW83rdfjxxSkCf1FDv4CnXgKziHKzgEXrXkDeRSEV1a8E", Base58.encode(BIP.wif(BIP0032.toPrivateKey(xprv_196_761273_0_516717))));
+        assertEquals("L4vDVKWMW83rdfjxxSkCf1FDv4CnXgKziHKzgEXrXkDeRSEV1a8E", Base58.encode(BIP.wif(prv_196_761273_0_516717)));
+        assertEquals("E5B2FF7C80F9D50AE33CD67CE7A6EDD604FCF6A0EA98CC52E2955E79601B05C1", BaseEncoding.base16().encode(prv_196_761273_0_516717));
     }
 
 }
